17dfe9e57335db8c915e0edf020e1ad5083829d5
krisfiction <krisfiction@gmail.com>
2020-05-29 14:51:37 -0400

    added room to screen

---
+++ DungeonMap/Map.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DungeonMap
+{
+    public class Map
+    {
+        string Wall_X = "-";
+        string Wall_Y = "|";
+        string Floor = ".";
+
+        string[,] GameMap = new string[80, 25];
+
+        public void MapDisplay()
+        {
+            for (int x = 0; x <= 79; x++)
+            {
+                for (int y = 0; y <= 24; y++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(GameMap[x, y]);
+                }
+            }
+        }
+
+        public void CreateMap()
+        {
+            Random random = new Random();
+
+            int RoomHeight = random.Next(5, 8);
+            int RoomWidth = random.Next(7, 10);
+            int RoomPOSX = random.Next(0, 79 - RoomWidth - 1);
+            int RoomPOSY = random.Next(0, 24 - RoomHeight - 1);
+
+            for (int y = 0; y <= RoomHeight; y++)
+            {
+                for (int x = 0; x <= RoomWidth; x++)
+                {
+                    if (y == 0 || y == RoomHeight)
+                    {
+                        GameMap[RoomPOSX + x, RoomPOSY + y] = Wall_X;
+                    }
+                    else if (x == 0 || x == RoomWidth)
+                    {
+                        GameMap[RoomPOSX + x, RoomPOSY + y] = Wall_Y;
+                    }
+                    else
+                    {
+                        GameMap[RoomPOSX + x, RoomPOSY + y] = Floor;
+                    }
+
+                }
+            }
+        }
+
+    }
+}
